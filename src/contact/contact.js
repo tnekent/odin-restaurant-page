44c3892e4189a808c224b7dba73feda004772d84
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2025-05-16 23:07:00 +0800

    Add contact page for reservations

---
+++ src/contact/contact.js
@@ -0,0 +1,33 @@
+const createFormField = function (inputType, id, desc) {
+  const fieldContainer = document.createElement("p");
+  const input = document.createElement("input");
+  input.type = inputType;
+  input.id = id;
+  const label = document.createElement("label");
+  label.textContent = desc;
+  label.setAttribute("for", id);
+  fieldContainer.append(label, input);
+  return fieldContainer;
+}
+
+export default function () {
+  const divContent = document.querySelector("#content");
+
+  const headerContact = document.createElement("header");
+  const h1ContactLabel = document.createElement("h1");
+  h1ContactLabel.textContent = "Book a reservation";
+  headerContact.append(h1ContactLabel);
+
+  const formReservation = document.createElement("form");
+  const inputName = createFormField("text", "fullname", "Full Name");
+  const inputEmail = createFormField("email", "email", "Email");
+  const inputPhoneNumber = createFormField("tel", "phonenum", "Phone Number");
+  const inputDate = createFormField("date", "date", "Date");
+  const inputTime = createFormField("time", "time", "Time");
+  const btnReserve = document.createElement("button");
+  btnReserve.textContent = "Reserve";
+
+  formReservation.append(inputName, inputEmail, inputPhoneNumber, inputDate, inputTime, btnReserve);
+
+  divContent.append(headerContact, formReservation);
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import showHome from "./home/home.js";
 import showMenu from "./menu/menu.js"
-
+import showContact from "./contact/contact.js"
 // showHome();
-showMenu();
+// showMenu();
+showContact();
