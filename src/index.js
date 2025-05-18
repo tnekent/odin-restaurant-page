55acd0357ba67f473daa21d9097671b769666172
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2025-05-18 19:37:36 +0800

    Add tab switching

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import showHome from "./home/home.js";
 import showMenu from "./menu/menu.js"
 import showContact from "./contact/contact.js"
-// showHome();
-// showMenu();
-showContact();
+
+const showTabsFns = {
+  home: showHome,
+  menu: showMenu,
+  contact: showContact
+}
+let currentTabName;
+
+const divContent = document.querySelector("#content");
+const btnTabSwitchList = document.querySelectorAll("nav>button");
+btnTabSwitchList.forEach(btnTab => {
+  btnTab.addEventListener("click", function () {
+    const tabName = btnTab.textContent.toLowerCase();
+    if (currentTabName === tabName) {
+      return;
+    }
+    divContent.innerHTML = "";
+    showTabsFns[tabName]();
+    currentTabName = tabName;
+  })
+})
+
+showHome();
+currentTabName = "home";
